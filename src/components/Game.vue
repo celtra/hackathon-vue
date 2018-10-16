e3b359bfd3a1c029715e64cdc1806b15129fec86
zan <zan.kusterle@gmail.com>
2018-10-16 14:23:54 +0200

    action component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -8,6 +8,10 @@
         <div class="jesus">
             <img src="../assets/jesus.jpeg">
             <progress :value="jesusHealth" max="1000"/>
+
+            <div class="actions">
+                <action name="Jesus smite" :cooldown="8" />
+            </div>
         </div>
     </div>
 </template>
@@ -16,6 +20,9 @@
 import Action from './Action.vue'
 
 export default {
+    components: {
+        Action,
+    },
     data () {
         return {
             devilHealth: 1000,
