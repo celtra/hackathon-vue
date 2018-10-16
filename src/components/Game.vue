a32cb1727f975e9a33ec1dea0d6709a82f08b18f
zan <zan.kusterle@gmail.com>
2018-10-16 14:22:05 +0200

    jesus and devil health

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,27 @@
 <template>
-    <div></div>
+    <div class="game">
+        <div class="devil">
+            <img src="../assets/devil.jpg">
+            <progress :value="devilHealth" max="1000"/>
+        </div>
+
+        <div class="jesus">
+            <img src="../assets/jesus.jpeg">
+            <progress :value="jesusHealth" max="1000"/>
+        </div>
+    </div>
 </template>
 
 <script>
 import Action from './Action.vue'
 
 export default {
+    data () {
+        return {
+            devilHealth: 1000,
+            jesusHealth: 1000,
+        }
+    }
 }
 </script>
 
