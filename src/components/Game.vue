62724957879ac7a4bec9050839bfa7184a69b251
zan <zan.kusterle@gmail.com>
2018-10-16 14:20:13 +0200

    base code

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,15 +1,5 @@
 <template>
-    <div class="game">
-        <div class="devil">
-            <img src="../assets/devil.jpg">
-            <progress :value="devilHP" max="1000"/>
-        </div>
-
-        <div class="jesus">
-            <img src="../assets/jesus.jpeg">
-            <progress :value="godHP" max="1000"/>
-        </div>
-    </div>
+    <div></div>
 </template>
 
 <script>
