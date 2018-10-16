b0e03ecb464ce1ab23f01701e25c647921ef93b4
zan <zan.kusterle@gmail.com>
2018-10-16 14:30:53 +0200

    add more abilities

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -11,9 +11,10 @@
         <div :class="jesusAnimation" class="jesus">
             <img src="../assets/jesus.jpeg">
             <progress :value="jesusHealth" max="1000"/>
-
             <div class="actions">
                 <action name="Jesus smite" :cooldown="8" @use="smite" />
+                <action name="Hear my prayer" :cooldown="8" @use="pray" />
+                <action name="Jesus dodge" :cooldown="8" @use="dodge" />
             </div>
         </div>
     </div>
@@ -32,14 +33,19 @@ export default {
             jesusHealth: 1000,
             jesusAnimation: '',
             isDevilCasting: false,
+            isJesusDodging: false,
         }
     },
     mounted () {
         setInterval(() => {
             this.isDevilCasting = true
             setTimeout(() => {
-                this.jesusHealth -= 100
-                this.isDevilCasting = false
+                if (this.isDevilCasting) {
+                    if (!this.isJesusDodging) {
+                        this.jesusHealth -= 100
+                    }
+                    this.isDevilCasting = false
+                }
             }, 1000)
         }, 3000)
     },
@@ -48,7 +54,20 @@ export default {
             this.devilHealth -= 80
             this.jesusAnimation = 'smite'
             setTimeout(() => this.jesusAnimation = '', 250)
-        }
+        },
+        pray () {
+            this.isDevilCasting = false
+            this.jesusAnimation = 'pray'
+            setTimeout(() => this.jesusAnimation = '', 1000)
+        },
+        dodge () {
+            this.isJesusDodging = true
+            this.jesusAnimation = 'dodge'
+            setTimeout(() => {
+                this.jesusAnimation = ''
+                this.isJesusDodging = false
+            }, 800)
+        },
     }
 }
 </script>
